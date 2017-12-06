64f9b42a90bbf6ae66c0fdc9aa775577803eba93
yukun <15152347277@163.com>
2017-12-06 17:41:29 +0800

    Linux部署文件

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'you-guess'
@@ -28,8 +27,17 @@ class Config(object):
     # 管理员简介
     ADMIN_PROFILE = '克制力，执行力'
 
-    WHOOSHEE_MIN_STRING_LEN = 1
+    # email
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIN_PASSWORD')
+    MAIL_SERVER = os.getenv('MAIL_SERVER') or 'smtp.163.com'
+    MAIL_PORT = os.getenv('MAIL_PORT') or '25'
 
+    ADMIN_MAIL_SUBJECT_PREFIX = 'blog'
+    ADMIN_MAIL_SENDER = 'admin 15152347277@163.com'
+    ADMIN_MAIL = os.getenv('ADMIN_MAIL')
+
+    WHOOSHEE_MIN_STRING_LEN = 1
 
     @staticmethod
     def init_app(app):
@@ -45,7 +53,42 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/mydb'
+    DEBUG = False
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        # 把错误发给管理
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.ADMIN_MAIL_SENDER,
+            toaddrs=[cls.ADMIN_MAIL],
+            subject=cls.ADMIN_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+class UnixConfig(ProductionConfig):
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.INFO)
+        app.logger.addHandler(syslog_handler)
 
 
 config = {
--- manage.py
+++ manage.py
@@ -9,6 +9,11 @@ app = create_app(os.getenv('CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+if os.path.exists('.env'):
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
 
 def make_shell_context():
     return dict(app=app, db=db, Admin=Admin, Post=Post, Tag=Tag, \
