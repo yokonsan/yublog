450b57379e95e49df823e18b83796905bb22f71c
Blackyukun <15152347277@163.com>
2021-02-28 00:29:07 +0800

    Reset the structure

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=yublog
+FLASK_ENV=development
+FLASK_DEBUG=1
--- mysql/init_database.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS `mydb`;
-CREATE DATABASE `mydb` default character set utf8 collate utf8_general_ci;
-
-use mydb;
-
--- Add your sql below
